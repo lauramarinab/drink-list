d68c988b96a97ec14e13b323f5563f07b14f8fc4
Laura Marina Bianchi <lauramarinabianchi@gmail.com>
2020-06-18 12:52:51 +0200

    Updates

---
+++ precache-manifest.548599850fa8a558991f132ab7ad6a20.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9583ffb10ae0efee7b59491610677014",
+    "url": "/drink-list/index.html"
+  },
+  {
+    "revision": "5fa48858989bd931e50d",
+    "url": "/drink-list/static/css/main.7d14095b.chunk.css"
+  },
+  {
+    "revision": "2c45b0643cc358ac1dbf",
+    "url": "/drink-list/static/js/2.b446bd21.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/drink-list/static/js/2.b446bd21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa48858989bd931e50d",
+    "url": "/drink-list/static/js/main.1bd0e03a.chunk.js"
+  },
+  {
+    "revision": "05d242f518e3f50e2a59",
+    "url": "/drink-list/static/js/runtime-main.42ff1f2f.js"
+  },
+  {
+    "revision": "8b75a8b80676f7a4baf3bfc2b3bfe99e",
+    "url": "/drink-list/static/media/drink-logo.8b75a8b8.svg"
+  },
+  {
+    "revision": "45d4a23cab8a14828f5c5cb2ea5f4005",
+    "url": "/drink-list/static/media/icon.45d4a23c.ttf"
+  },
+  {
+    "revision": "8b1e8f9b939c58ec20da00c64c4dfc2d",
+    "url": "/drink-list/static/media/icon.8b1e8f9b.eot"
+  },
+  {
+    "revision": "d446f13985a1d83639a021d7bfb5b2a5",
+    "url": "/drink-list/static/media/icon.d446f139.woff"
+  },
+  {
+    "revision": "e277ac88ca9c50200032f342175f632c",
+    "url": "/drink-list/static/media/icon.e277ac88.woff2"
+  },
+  {
+    "revision": "fed137def58cf24f793f2a210b676246",
+    "url": "/drink-list/static/media/icon.fed137de.svg"
+  },
+  {
+    "revision": "b90c46814a8aa18e4c5c70d203a2eda2",
+    "url": "/drink-list/static/media/order-icon.b90c4681.svg"
+  }
+]);
